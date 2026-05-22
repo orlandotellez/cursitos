42e011706185cce1a117a3795fc8e6d5b9622ea5
destripador2000 <davidhumbertop503@gmail.com>
2026-05-22 07:23:26 -0600

    Feat: Creación de enum para los estados de pagos, compilación éxitosa

---
+++ backend/src/Domain/Enums/PaymentStatus.cs
@@ -0,0 +1,10 @@
+namespace Cursinet.Domain.Enums;
+
+
+// Creación de enum para estado de pagos
+public enum PaymentStatus{
+	Pending, // Pago iniciado, pendiente de confirmación
+	Completed, // Pago confirmado y completado
+	Failed, // Pago rechazado o fallido
+	Refunded // Pago reembolsado
+}
