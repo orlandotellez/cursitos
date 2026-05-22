1a7402c85fb21074b1d3f3f06cb524b466facfff
destripador2000 <davidhumbertop503@gmail.com>
2026-05-22 07:07:32 -0600

    Feat: Creación de enum para los tipoś de niveles de los cursos, compilación éxitosa

---
+++ backend/src/Domain/Enums/CourseLevel.cs
@@ -0,0 +1,9 @@
+namespace test.Domain.Enums;
+
+// Creación de enum para los niveles del curso
+public enum CourseLevel{
+	Begginer, // Principiante-Sin conocimientos previos
+	Intermediate, // Conocimientos básicos
+	Advanced, // Avanzado-Dominio del tema
+	Expert // Experto-Nivel Profesional
+}
