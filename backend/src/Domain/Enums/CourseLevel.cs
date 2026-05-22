d0cd521327f76bf76d31fe5becc000f14f80ea3a
orlandotellez <espadachinkinno@gmail.com>
2026-05-22 08:53:54 -0600

    fix: agregar nombre de app en el import y tambien formatear bien los enums

--- backend/src/Domain/Enums/CourseLevel.cs
+++ backend/src/Domain/Enums/CourseLevel.cs
@@ -1,9 +1,10 @@
-namespace test.Domain.Enums;
+namespace Curisnet.test.Domain.Enums;
 
 // Creación de enum para los niveles del curso
-public enum CourseLevel{
-	Begginer, // Principiante-Sin conocimientos previos
-	Intermediate, // Conocimientos básicos
-	Advanced, // Avanzado-Dominio del tema
-	Expert // Experto-Nivel Profesional
+public enum CourseLevel
+{
+    Begginer, // Principiante-Sin conocimientos previos
+    Intermediate, // Conocimientos básicos
+    Advanced, // Avanzado-Dominio del tema
+    Expert // Experto-Nivel Profesional
 }
--- backend/src/Domain/Enums/UserRole.cs
+++ backend/src/Domain/Enums/UserRole.cs
@@ -1,9 +1,10 @@
 namespace Cursinet.Domain.Enums;
 
 // Creación de enums para los roles de usuario
-public enum UserRole{
-	Student, // Estudiante-Consumidor de recursos
-	Instructor, // Instructor-crea y gestiona recursos
-	Admin, // Administrador-Control total
-	Moredator // Moderador-modera contenido 
+public enum UserRole
+{
+    Student, // Estudiante-Consumidor de recursos
+    Instructor, // Instructor-crea y gestiona recursos
+    Admin, // Administrador-Control total
+    Moderator // Moderador-modera contenido 
 }
